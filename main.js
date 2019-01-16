d2f0d4f1de8e122d42d221ef957afa240995b9e8
ray-nis <reirei256@gmail.com>
2019-01-16 06:06:43 +0100

    js

--- main.js
+++ main.js
@@ -20,27 +20,55 @@ const players = (() => {
 })();
 
 const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    let board = ["", "", "", "", "", "", "", "", ""];
+    let win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
 
     const checkWin = () => {
-        let win = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-
         for (let i = 0; i < win.length; i++) {
                 if (board[win[i][0]] === "X" && board[win[i][1]] === "X" && board[win[i][2]] === "X") {
-                    console.log("x wins");
+                    showWinner(i);
                 }
                 else if (board[win[i][0]] === "O" && board[win[i][1]] === "O" && board[win[i][2]] === "O") {
-                    console.log("o wins");
+                    showWinner(i);
                 }
+                else if (board.join("").length  === 9) {
+                    showWinner(-1);
+                }
+        }
+    };
+
+    const showWinner = (index) => {
+        if (index === -1) {
+            document.querySelector("#result").innerText = "Draw";
+            document.querySelector("#result-div").style.display = "block";
+        }
+        else {
+            let winner = document.querySelectorAll(".n");
+            winner[win[index][0]].style.color = "#42e814";
+            winner[win[index][1]].style.color = "#42e814";
+            winner[win[index][2]].style.color = "#42e814";
+            document.querySelector("#result").innerText = "X wins";
+            document.querySelector("#result-div").style.display = "block";
+        }
+    };
+
+    const restart = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+        show.innerHTML = "";
+        renderBoard();
+        document.querySelector("#result-div").style.display = "none";
+        if (players.getCurrentTurn() === "O") {
+            players.changeTurn();
         }
     };
 
@@ -49,6 +77,7 @@ const gameBoard = (() => {
             let div = document.createElement("div");
             div.innerHTML = board[i];
             div.setAttribute("id", i);
+            div.setAttribute("class", "n");
             div.addEventListener("click",(e) => {
                 if (board[e.target.id] === "X" || board[e.target.id] === "O") {
 
@@ -62,6 +91,10 @@ const gameBoard = (() => {
             })
             show.appendChild(div);
         }
+
+        document.querySelector("#restart").addEventListener("click", ()=>{
+            restart();
+        })
     }
     return {renderBoard, checkWin};
 })();
